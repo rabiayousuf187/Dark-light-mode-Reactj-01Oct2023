1e8f9f9c8df65b9620ef5ddea79690bb3c0c9ea3
rabiayousuf187 <rabiayousuf187@gmail.com>
2023-10-09 11:00:21 +0500

    NAvbar added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import logo from './logo.svg';
 import {Container} from 'reactstrap'
+import NavbarComp from './components/NavbarComp/NavbarComp'
 import './App.css';
+
 
 function App() {
   return (
