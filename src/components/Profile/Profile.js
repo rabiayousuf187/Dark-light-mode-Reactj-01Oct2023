7dc4409bfdc263164df9e7ab9d0ca773b17b3f0b
rabiayousuf187 <rabiayousuf187@gmail.com>
2023-10-13 15:20:27 +0500

    Start Working on Social Link Sec.

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,34 +1,39 @@
 import React from 'react';
+import { Button, Container } from 'reactstrap';
 // import user from '../../img/user.jpg'
 
 const Profile = (args) => {
     return (
         <div {...args}>
-            <div className='info'>
-                <div className='profile-img'>
-                    {/* <img className='user-img' src={user} alt='avatar' /> */}
-                </div>
-                <div className='details'>
-                    <h2 className='username'>Christian Louboutin</h2>
-                    <h4 className='job-title'>Front End Developer</h4>
+            <Container>
+                <div className='info'>
+                    <div className='profile-img'>
+                        {/* <img className='user-img' src={user} alt='avatar' /> */}
+                    </div>
+                    <div className='details'>
+                        <h2 className='username'>Christian Louboutin</h2>
+                        <h4 className='job-title'>Front End Developer</h4>
 
+                    </div>
                 </div>
-            </div>
-            <div className='social-link'>
-                <button href="https://www.facebook.com/"><i class="bi bi-twitter"></i></button>
-                <button href="https://www.facebook.com/"><i class="bi bi-facebook"></i></button>
-                <button href="https://www.instagram.com/"><i class="bi bi-instagram"></i></button>
-            </div>
-            <div className='about'>
-                <p>
-                    A front-end web developer is a crucial part of the web development team responsible for creating the user interface and ensuring a seamless user experience on websites and web applications. Their role involves turning design concepts into responsive, interactive, and visually appealing web pages.
-                </p>
-            </div>
-            <div className='portfolio'>
-                <button href="https://www.facebook.com/"><i class="bi bi-twitter"></i>STUDIO</button>
-                <button href="https://www.facebook.com/"><i class="bi bi-facebook"></i>WORK</button>
-                <button href="https://www.instagram.com/"><i class="bi bi-instagram"></i>FAVORITE</button>
-            </div>
+                <div className='social-link'>
+                    <Button href="https://www.facebook.com/"><i class="bi bi-twitter"></i></Button>
+                    <Button href="https://www.facebook.com/"><i class="bi bi-facebook"></i></Button>
+                    <Button href="https://www.instagram.com/"><i class="bi bi-instagram"></i></Button>
+                </div>
+                <div className='about'>
+                    <p>
+                        A front-end web developer is a crucial part of the web development team responsible for creating the user interface and ensuring a seamless user experience on websites and web applications. Their role involves turning design concepts into responsive, interactive, and visually appealing web pages.
+                    </p>
+                </div>
+                <div className='portfolio'>
+                    <button className='btn' href="https://www.facebook.com/"><i class="bi bi-twitter"></i>STUDIO</button>
+                    <button className='btn' href="https://www.facebook.com/"><i class="bi bi-facebook"></i>WORK</button>
+                    <button className='btn' href="https://www.instagram.com/"><i class="bi bi-instagram"></i>FAVORITE</button>
+                </div>
+
+
+            </Container>
         </div >
     )
 }
