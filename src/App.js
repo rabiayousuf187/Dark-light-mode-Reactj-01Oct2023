c7ad3201edbd16a99f20fb21a11db96a8567fd27
rabiayousuf187 <rabiayousuf187@gmail.com>
2023-10-14 09:28:29 +0500

    theme completed working on dark and light

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Profile from './components/Profile/Profile';
 // https://images.unsplash.com/photo-1498050108023-c5249f4df085?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1352&q=80
 
 function App() {
+  const [set]
   return (
     <div className="App">
       <Container className='banner' fluid>
