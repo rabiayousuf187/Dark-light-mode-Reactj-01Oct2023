8efaba0fe0baa4288df641e14f08c549517e764d
rabiayousuf187 <rabiayousuf187@gmail.com>
2023-10-14 09:33:28 +0500

    Workikng on dark and light theme

--- src/App.js
+++ src/App.js
@@ -3,11 +3,18 @@ import { Container } from 'reactstrap'
 import NavbarComp from './components/NavbarComp/NavbarComp'
 import './App.css';
 import Profile from './components/Profile/Profile';
+import { useState } from 'react';
 
 // https://images.unsplash.com/photo-1498050108023-c5249f4df085?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1352&q=80
 
 function App() {
-  const [set]
+  const [theme , setTheme] = useState({color: 'black' , color: 'backgroundColor'});
+
+  let changeTheme = () =>{
+    setTheme(theme.backgroundColor = 'white' ? {backgroundColor: 'black' , color: 'smokewhite'} : 
+
+    )
+  }
   return (
     <div className="App">
       <Container className='banner' fluid>
